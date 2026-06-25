36a8cc2e4fe8eeb32d23cc5cdf8d2d46652db652
Dan Marshall <danmar@microsoft.com>
2026-06-25 14:44:59 -0700

    Add Vite configuration for DOM builds with minified output

---
+++ nodejs/packages/treebark/vite.browser.dom.min.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'browser',
+    emptyOutDir: false, // Don't clear the directory, as we're adding to it
+    lib: {
+      entry: 'src/dom.ts',
+      name: 'Treebark',
+      formats: ['umd'],
+      fileName: () => 'treebark-dom-browser.min.js'
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        extend: true,
+        globals: {}
+      }
+    },
+    sourcemap: true,
+    minify: true
+  }
+});
